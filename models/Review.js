8ff19093f27ed90af291fc5f4e59f6f300696f0e
Jaxer24 <jacobarciniega24@gmail.com>
2025-05-05 15:05:55 -0700

    Adding a few useful model ideas to work off of

--- models/Review.js
+++ models/Review.js
@@ -1,11 +1,11 @@
 /**
- * User model
+ * Review model
  * Defines the schema for users in our application
  */
 const mongoose = require('mongoose');
 
 const ReviewSchema = new mongoose.Schema({
-  
+  //creates review title attribute
   title: {
     type: String,
     required: [true, 'Tile is required'],
@@ -13,16 +13,31 @@ const ReviewSchema = new mongoose.Schema({
     minlength: [1, 'Title must be at least 5 characters'],
     maxlength: [30, 'Title cannot exceed 30 characters']
   },
+  //creates review message attribute
   message: {
     type: String,
     required: [true, 'Message is required'],
     // Removed index to avoid creation issues
   },
+  //creates user attribute to connect the author to this review
+  userID: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' ,
+    required: [true, 'Message is required'],
+  },
+  //creates movie attribute to connect the movie to this review
+  movieID: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Movie' ,
+    required: [true, 'Message is required'],
+  },
+  //creates rating attribute
   rating: {
     type: Number, 
     required: [true, 'Rating is required'],
     max: 5
   },
+  //creates a date attribute
   createdAt: {
     type: Date,
     default: Date.now
--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
+  //creates username attribute
   username: {
     type: String,
     required: [true, 'Username is required'],
@@ -14,6 +15,7 @@ const UserSchema = new mongoose.Schema({
     minlength: [3, 'Username must be at least 3 characters'],
     maxlength: [20, 'Username cannot exceed 20 characters']
   },
+  //creates email attribute
   email: {
     type: String,
     required: [true, 'Email is required'],
@@ -22,19 +24,27 @@ const UserSchema = new mongoose.Schema({
     lowercase: true,
     match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address']
   },
+  //creates password attribute
   password: {
     type: String,
     required: [true, 'Password is required'],
     minlength: [8, 'Password must be at least 8 characters']
   },
+  //creates image boolean attribute
   hasProfileImage: {
     type: Boolean,
     default: false
   },
+  //creates a date attribute
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  //Adds an ArrayList of type movie to store users favorite movies
+  favorites: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Movie' 
+  }]
 }, {
   // Add virtual properties when converting to JSON
   toJSON: { virtuals: true },
